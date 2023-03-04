13c6b9fa22ff68c95a81958c8c86a0a78821ba83
JuliaChe <chebykina.yulya28@bk.ru>
2023-03-04 13:01:18 +0300

    Initial commit

---
+++ src/test/java/MoviesManagerTest.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoviesManagerTest {
+
+
+    @Test
+    public void addMovie() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void overLimit() {
+        MoviesManager manager = new MoviesManager(5);
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+
+        int expected = 5;
+        int actual = manager.findLast().length;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lessThanLimit() {
+        MoviesManager manager = new MoviesManager(5);
+        manager.add("Film 11");
+        manager.add("Film 11");
+        manager.add("Film 11");
+
+        int expected = 3;
+        int actual = manager.findLast().length;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void sortTest() {
+        MoviesManager manager = new MoviesManager();
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
+
