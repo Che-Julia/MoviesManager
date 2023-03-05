98226eabe2b4478112132153b08b613aaf39cf25
JuliaChe <chebykina.yulya28@bk.ru>
2023-03-05 11:26:11 +0300

    fixed the test "overLimit"

--- src/test/java/MoviesManagerTest.java
+++ src/test/java/MoviesManagerTest.java
@@ -22,31 +22,32 @@ public class MoviesManagerTest {
     @Test
     public void overLimit() {
         MoviesManager manager = new MoviesManager(5);
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
+        manager.add("Film 5");
+        manager.add("Film 6");
+        manager.add("Film 7");
+        manager.add("Film 8");
+        manager.add("Film 9");
+        manager.add("Film 10");
 
-        int expected = 5;
-        int actual = manager.findLast().length;
+        String[] expected = {"Film 10", "Film 9", "Film 8", "Film 7", "Film 6"};
+        String[] actual = manager.findLast();
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void lessThanLimit() {
         MoviesManager manager = new MoviesManager(5);
-        manager.add("Film 11");
-        manager.add("Film 11");
-        manager.add("Film 11");
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
 
-        String[] expected = {"Film 11", "Film 11", "Film 11"};
+        String[] expected = {"Film 4", "Film 3", "Film 2", "Film 1"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
